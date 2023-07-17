2060ffc3b9a05e3baf8fffebe5225d1c850d02e9
Ian/Partner <i.lassandro@ufl.edu>
2023-07-17 16:45:06 -0400

    Added "def main" which declares a few menu variables, prints menu screen with only option that works being the quit option. moved the initial hello world in the file  to the end of the menu code and set up the if statement to run the main program.

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
-# this is my first github addition
+def main():
+    # Declaring var
+    num1 = 1
+    num2 = 2
+    num3 = 3
 
-print("hello world")
+    # Print menu
+    print("Menu")
+    print("-------------")
+    print(f"{num1}. Encode")
+    print(f"{num2}. Decode")
+    print(f"{num3}. Quit\n")
+
+    # Get user input
+    option = input("Please enter an option: ")
+
+    if option == num1:
+        encode_pass = input("Please enter your password to encode")
+        pass
+    elif option == num2:
+        decode_pass = input("The encoded password is {}, and the original password is {}.\n")
+        pass
+    elif option == num3:
+        quit()
+
+    # this is my first GitHub addition
+
+    print("hello world")
+
+
+if __name__ == '__main__':
+    main()

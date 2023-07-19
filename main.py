3cb61d5e2711ea51f2f35e6beae07c3f0caa471e
Karina Garridolee <kgarridolee@gmail.com>
2023-07-19 02:32:24 -0400

    added decode function and edited the decoder option

--- main.py
+++ main.py
@@ -6,7 +6,14 @@ def password_encoder(data):
     return result
     pass
 
+
 # Did the encoder, believe the decoder should just be the inverse of encoder
+def password_decoder(data):
+    result = ''
+    for digit in data:
+        decoded = str((int(digit) - 3) % 10)
+        result += decoded
+    return result
 
 
 def main():
@@ -33,8 +40,8 @@ def main():
             pass
         elif option == num2:
             # Possible issue with print statement if passed before choosing option 1
-            print(f"The encoded password is", pass_encoded, ", and the original password is"
-                                                            '''Need decoded part here''' ".\n")
+            pass_decoded = password_decoder(pass_encoded)
+            print(f"The encoded password is {pass_encoded} and the original password is {pass_decoded}.\n")
             pass
         elif option == num3:
             break
